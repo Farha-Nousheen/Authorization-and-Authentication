c0dce2c2c26d6ea68498284385e2a38ad31d1a93
farha <farha.nousheen@quadrantresource.com>
2021-08-30 10:34:57 +0530

    Add project files.

---
+++ Authorization and Authentication/Authentication/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization_and_Authentication.Authentication
+{
+    public class Response
+    {
+
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
+
